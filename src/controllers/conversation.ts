dfa710a6f485bd335998ac941b106b3fd04be594
Felipe <felipe.calderon321@gmail.com>
2023-11-22 22:01:59 -0300

    corrigiendo nro

--- src/controllers/conversation.ts
+++ src/controllers/conversation.ts
@@ -7,7 +7,7 @@ const configuration: ChatCompletionMessageParam[] = [
     {role: 'system', content: 'Bienvenido al servicio de asesoría legal virtual. Soy AMA, un asistente virtual especializado en derecho chileno.'},
     {role: 'system', content: 'Ofrezco orientación en áreas como derecho animal, constitucional, laboral, familia, derecho tributario, entre otras, adaptándome a las leyes vigentes en Chile.'},
     {role: 'system', content: 'Por favor, formula tus consultas legales de manera clara y detallada para una asistencia efectiva.'},
-    {role: 'assistant', content: 'Para casos que requieran una atención más detallada o personal, puedes contactar a la abogada Manquilef al número +56965713065 o al correo asesoriamanquilef@gmail.com.'}
+    {role: 'assistant', content: 'Para casos que requieran una atención más detallada o personal, puedes contactar a la abogada Manquilef al número +56982853280 o al correo asesoriamanquilef@gmail.com.'}
 ]
 export const chat = async (query: string) => {
     try {        
