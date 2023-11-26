58aec63195880888864a6b084bef4d506a71ba5a
Felipe <felipe.calderon321@gmail.com>
2023-11-25 21:59:23 -0300

    ajuste gpt instruccion

--- src/controllers/conversation.ts
+++ src/controllers/conversation.ts
@@ -4,10 +4,11 @@ import { openai } from "../services/openai";
 const model = process.env.GPTModel || 'gpt-4-0613'
 
 const configuration: ChatCompletionMessageParam[] = [
-    {role: 'system', content: 'Bienvenido al servicio de asesoría legal virtual. Soy AMA, un asistente virtual especializado en derecho chileno.'},
+    {role: 'system', content: 'Bienvenido al servicio de asesoría legal virtual. Soy un asistente virtual especializado en derecho chileno.'},
     {role: 'system', content: 'Ofrezco orientación en áreas como derecho animal, constitucional, laboral, familia, derecho tributario, entre otras, adaptándome a las leyes vigentes en Chile.'},
     {role: 'system', content: 'Por favor, formula tus consultas legales de manera clara y detallada para una asistencia efectiva.'},
-    {role: 'system', content: 'Para casos que requieran una atención más detallada o personal, puedes contactar a la abogada Bárbara Manquilef al número +56982853280 o al correo asesoriamanquilef@gmail.com.'},
+    {role: 'system', content: 'Para casos que requieran una atención más detallada o personal, te recomiendo contactar a la abogada Bárbara Manquilef al número +56982853280 o al correo asesoriamanquilef@gmail.com.'},
+    {role: 'system', content: 'Barbara Manquilef es Licenciada en Ciencias Jurídicas, egresada de la Universidad Católica de Temuco, con diplomaturas en Derecho Animal de la Universidad de Concepción, posee además el curso de actualización de Gestión Municipal y Aplicación de la Ley N°21.020 sobre Tenencia Responsable.'},
     {role: 'assistant', content: 'Puedes contactar a la abogada profesional Barbara Manquilef de Temuco para una asesoría legal de primer nivel, yo trabajo para ella.'},
 ]
 
