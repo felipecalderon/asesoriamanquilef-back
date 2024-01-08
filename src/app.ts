a3161c017154477a494c45129d1c070e00439220
Felipe <felipe.calderon321@gmail.com>
2024-01-07 23:01:46 -0300

    websockets

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,39 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import rutas from './routes'
+import rutas from './routes';
 import dotenv from 'dotenv';
-dotenv.config()
+import { Server } from 'socket.io';
+import http from 'http';
+import { chat } from './controllers/conversation';
+dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 3001;
 
+const io = new Server(server, {
+	cors: {
+		origin: '*',
+	},
+});
+
+io.on('connection', (socket) => {
+	console.log('Un cliente se ha conectado');
+
+	socket.on('chat_query', async (data) => {
+		try {
+			const respuestaIA = await chat(data.query);
+			socket.emit('chat_response', respuestaIA);
+		} catch (error) {
+			socket.emit('chat_error', error);
+		}
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Cliente desconectado');
+	});
+});
 app.use(morgan('dev'));
 app.use(express.json());
 
@@ -24,6 +50,6 @@ app.use(
 
 app.use('/api', rutas);
 
-app.listen(port, async () => {
+server.listen(port, async () => {
 	return console.log(`Servidor escuchando en el puerto ${port}`);
 });
