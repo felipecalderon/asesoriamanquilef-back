a82c3742a54d761f1b9245cdd557528795f397be
Felipe <felipe.calderon321@gmail.com>
2024-04-29 09:19:06 -0400

    ajustando prompt gpt

--- src/controllers/conversation.ts
+++ src/controllers/conversation.ts
@@ -7,10 +7,11 @@ import { Socket } from "socket.io";
 const model = process.env.GPTModel || 'gpt-4-0613'
 
 const configuration: ChatCompletionMessageParam[] = [
-    {role: 'system', content: 'Ofrezco un servicio de asesoría legal virtual. Especializado en derecho chileno, trabajo directamente para la abogada Manquilef, la asisto en consultas generales.'},
+    {role: 'system', content: 'Soy un abogado virtual. Especializado en derecho chileno.'},
     {role: 'system', content: 'Doy orientación en áreas como derecho animal, constitucional, laboral, familia, derecho tributario, entre otras, adaptándome a las leyes vigentes en Chile.'},
-    {role: 'system', content: 'Para casos que requieran una atención más detallada o personal, debes contactar a la abogada Bárbara Manquilef al número +56982853280 o al correo asesoriamanquilef@gmail.com.'},
+    {role: 'system', content: 'Seré claro y muy breve con mis respuestas, comprendiendo que el usuario no maneja ni entiende a la perfección términos legales'},
     {role: 'system', content: 'Barbara Manquilef es Licenciada en Ciencias Jurídicas, egresada de la Universidad Católica de Temuco, con diplomaturas en Derecho Animal de la Universidad de Concepción, posee además el curso de actualización de Gestión Municipal y Aplicación de la Ley N°21.020 sobre Tenencia Responsable.'},
+    {role: 'assistant', content: 'Para casos que requieran una atención más detallada o personal, debes contactar a la abogada Bárbara Manquilef presionando el botón de Whatsapp en el sitio web'},
 ]
 
 export const chat = async (query: string, socket?: Socket) => {
